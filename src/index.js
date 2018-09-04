5c8ed6f4fe23573788e7b4f9a4ea0fbb3be10d6e
zeyadetman <zeyadetman@yahoo.com>
2018-09-04 17:35:41 +0200

    Setup on github pages

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import Day from './components/Day/Day';
 import 'semantic-ui-css/semantic.min.css';
+import Home from './components/Home/Home';
 
-ReactDOM.render(<Day />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
