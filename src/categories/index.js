ac518dfa60cb15fcb9aa87c3cca7ead7d43e6bc9
Ahmed <ahmed.samer@cis.asu.edu.eg>
2018-09-06 01:41:18 +0200

    solve images problem

--- src/categories/index.js
+++ src/categories/index.js
@@ -1,19 +1,19 @@
 export const expenses = [
-    {text: 'bills', value: 'bills', image: {avatar: true, src: '/images/categories/bills.png'}},
-    {text: 'clothes', value: 'clothes', image: {avatar: true, src: '/images/categories/clothes.png'}},
+    {text: 'bills', value: 'bills', image: {avatar: true, src: 'images/categories/bills.png'}},
+    {text: 'clothes', value: 'clothes', image: {avatar: true, src: 'images/categories/clothes.png'}},
     {text: 'communication', value: 'communication', image: {avatar: true, src: '/images/categories/communication.png'}},
-    {text: 'eating out', value: 'eatingOut', image: {avatar: true, src: '/images/categories/eatingOut.png'}},
-    {text: 'entertainment', value: 'entertainment', image: {avatar: true, src: '/images/categories/entertainment.png'}},
-    {text: 'food', value: 'food', image: {avatar: true, src: '/images/categories/food.png'}},
-    {text: 'gifts', value: 'gifts', image: {avatar: true, src: '/images/categories/gifts.png'}},
-    {text: 'house', value: 'house', image: {avatar: true, src: '/images/categories/house.png'}},
-    {text: 'sports', value: 'sports', image: {avatar: true, src: '/images/categories/sports.png'}},
-    {text: 'transport', value: 'transport', image: {avatar: true, src: '/images/categories/transport.png'}},
-    {text: 'health', value: 'health', image: {avatar: true, src: '/images/categories/health.png'}},
+    {text: 'eating out', value: 'eatingOut', image: {avatar: true, src: 'images/categories/eatingOut.png'}},
+    {text: 'entertainment', value: 'entertainment', image: {avatar: true, src: 'images/categories/entertainment.png'}},
+    {text: 'food', value: 'food', image: {avatar: true, src: 'images/categories/food.png'}},
+    {text: 'gifts', value: 'gifts', image: {avatar: true, src: 'images/categories/gifts.png'}},
+    {text: 'house', value: 'house', image: {avatar: true, src: 'images/categories/house.png'}},
+    {text: 'sports', value: 'sports', image: {avatar: true, src: 'images/categories/sports.png'}},
+    {text: 'transport', value: 'transport', image: {avatar: true, src: 'images/categories/transport.png'}},
+    {text: 'health', value: 'health', image: {avatar: true, src: 'images/categories/health.png'}},
 ];
 
 export const income = [
-    {text: 'savings', value: 'savings', image: {avatar: true, src: '/images/categories/savings.png'}},
-    {text: 'salary', value: 'salary', image: {avatar: true, src: '/images/categories/salary.png'}},
-    {text: 'deposits', value: 'deposits', image: {avatar: true, src: '/images/categories/deposits.png'}},
+    {text: 'savings', value: 'savings', image: {avatar: true, src: 'images/categories/savings.png'}},
+    {text: 'salary', value: 'salary', image: {avatar: true, src: 'images/categories/salary.png'}},
+    {text: 'deposits', value: 'deposits', image: {avatar: true, src: 'images/categories/deposits.png'}},
 ]
